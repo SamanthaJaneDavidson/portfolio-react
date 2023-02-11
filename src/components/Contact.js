022312a0d2e5b090533fb37f7371aad8abaef50c
unknown <samanthajanedavidson@gmail.com>
2023-02-10 23:14:52 -0500

    Deleted potentially corrupted projects file upon advice from tutor

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,60 @@
-import React from "react";
+// import React, { useState } from 'react';
 
-function Contact() {
-    return (
-        <div>
-            For questions or more information please use the form below. 
-        </div>
-        
-    )
-}
 
-export default Contact;
+// function ContactForm() {
+
+//   const [ContactName, setContactName] = useState('');
+//   const [ContactEmail, setContactEmail] = useState('');
+//   const [ContactMessage, setContactMessage] = useState('');
+
+// //   const handleInputChange = (e) => {
+// //     // Getting the value and name of the input which triggered the change
+// //     const { name, value } = e.target;
+
+// //     // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
+// //     return name === 'firstName' ? setFirstName(value) : setLastName(value);
+// //   };
+
+//   const handleFormSubmit = (e) => {
+//     // Preventing the default behavior of the form submit (which is to refresh the page)
+//     e.preventDefault();
+
+//     // Alert the user their first and last name, clear the inputs
+//     // alert(`Hello ${firstName} ${lastName}`);
+//     // setFirstName('');
+//     // setLastName('');
+//   };
+
+//   return (
+//     <div>
+//       <form className="form">
+//         <input
+//           value={ContactName}
+//           name="ContactName"
+//           onChange={handleFormSubmit}
+//           type="text"
+//           placeholder="Name"
+//         />
+//         <input
+//           value={ContactEmail}
+//           name="ContactEmail"
+//           onChange={handleFormSubmit}
+//           type="text"
+//           placeholder="Email address"
+//         />
+//         <input
+//           value={ContactMessage}
+//           name="ContactMessage"
+//           onChange={handleFormSubmit}
+//           type="text"
+//           placeholder="Message"
+//         />
+//         <button type="button" onClick={handleFormSubmit}>
+//           Submit
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default ContactForm;
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
 
-function ProjectCard(project) {
+function ProjectCard(props) {
+    console.log(props)
     return (
         <div>
             <div className="card h-100" style={{width: '25rem'}} >
-                <img src={project.image} className="card-img-top" style={{width: "100%", height: "35vw", objectFit: "cover"}} alt={project.name} />
+                <img src={props.image} className="card-img-top" style={{width: "100%", height: "35vw", objectFit: "cover"}} alt={props.name} />
                 <div className="card-body">
-                    <h4 className="card-title">{project.name}</h4>
-                    <p className="card-text">{project.description}</p>
+                    <h4 className="card-title">{props.name}</h4>
+                    <p className="card-text">{props.description}</p>
                 </div>
             </div>
         </div>
     )
 }
+
 export default ProjectCard;
