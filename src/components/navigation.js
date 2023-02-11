7e686371a771699e7145f711082c10e08b3f907f
unknown <samanthajanedavidson@gmail.com>
2023-02-11 14:03:00 -0500

    updated nav bar; response design not working

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,22 +2,24 @@ import React from "react";
 
 function Navigation(props) {
     return (
-        <nav id="main-nav" className="links">
-            <ul>
-                <li className="nav-item active">
-                    <a className="nav-link" href="#AboutMe" onClick={() => props.handlePageChange('AboutMe')}>About Me <span className="sr-only"></span></a>
-                </li>
-                <li className="nav-item active">
-                    <a className="nav-link" href="#Projects" onClick={() => props.handlePageChange('Projects')}>Projects <span className="sr-only"></span></a>
-                </li>
-                <li className="nav-item active">
-                    <a className="nav-link" href="#Contact" onClick={() => props.handlePageChange('Contact')}>Contact<span className="sr-only"></span></a>
-                </li>
-                <li className="nav-item active">
-                    <a className="nav-link" href="#Resume" onClick={() => props.handlePageChange('Resume')}>Resume<span className="sr-only"></span></a>
-                </li>
-            </ul>
-    </nav>
+        <div className="navbar navbar-expand{-sm|-md|-lg|-xl}">
+            <nav id="main-nav" className="links">
+                <ul>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#AboutMe" onClick={() => props.handlePageChange('AboutMe')}>About Me <span className="sr-only"></span></a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#Projects" onClick={() => props.handlePageChange('Projects')}>Projects <span className="sr-only"></span></a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#Contact" onClick={() => props.handlePageChange('Contact')}>Contact<span className="sr-only"></span></a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#Resume" onClick={() => props.handlePageChange('Resume')}>Resume<span className="sr-only"></span></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
