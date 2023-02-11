3934a6db1eaf4aba28a59c6d4a22064e1710cf21
unknown <samanthajanedavidson@gmail.com>
2023-02-10 23:16:46 -0500

    recreated projects folder and added back in prior code; syntax errors appear to be gone now. Glad I tried to fix the old one for several hours

---
+++ src/components/Projects.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ProjectCard from 'ProjectCard';
+
+function Projects() {
+
+  const project = [
+    {
+      name: "Grrrrinder",
+      image: "./images/Grrrrinder_dogs_mainpage.jpg",
+      description: "Add description."
+    },
+    {
+      name: "Tres Tamales",
+      image: "./images/taco.png",
+      description: "Add description."
+    },
+    {
+        name: "Note Taker",
+        image: "./images/note_taker.png",
+        description: "Add description."
+      },
+      {
+        name: "Weather Dashboard",
+        image: "./images/weather_dashboard.png",
+        description: "Add description."
+      },
+      {
+        name: "Password Generator",
+        image: "./images/password_generator.png",
+        description: "Add description."
+      },
+      {
+        name: "Code Quiz",
+        image: "./images/code_quiz.png",
+        description: "Add description."
+      }
+]
+
+  return (
+     
+
+    <div className="row">
+        {project.map((project, index) => {
+            return (
+                <div className="col-sm align-items-stretch" key={index}>
+                <ProjectCard name={project.name} image={project.image} description={project.description} />
+                </div>
+            )
+        
+            })}
+    </div>   
+
+)
+}
+
+export default Projects;
